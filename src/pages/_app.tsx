3fbd161b8c174d07e5f2fcc0d215a311503e22c5
joao021 <joaoramalho30@yahoo.com.br>
2024-07-23 20:33:01 -0300

    chore: configs inciais projeto docker, e dependencias

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from "next/app";
+import GlobalStyle from "../styles/globalStyles";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
